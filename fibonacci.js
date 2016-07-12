f7a75c974ca75471a73a97833aa023220b3218c6
JoshSutcliffe <joshclement12@gmail.com>
2016-07-13 00:40:05 +0100

    changed var values

--- fibonacci.js
+++ fibonacci.js
@@ -2,11 +2,11 @@ console.log("connected");
 
 var checkFibbo = function(num) {
   var sum = 0;
-  var a = 0, b = 1, f = 1;
+  var p = 0, l = 1, f = 1;
   for(var i = 2; i < num; i++) {
-    f = a + b;
-    a = b;
-    b = f;
+    f = p + l;
+    p = l;
+    l = f;
     if (f > 4000000) {
       break
     } else if (f % 2 === 0) {
